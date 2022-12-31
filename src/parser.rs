5ee4425f1dccb5ea3eb451a958f2e06bae33a8c8
Joshua <joshua.jacobowitz@yahoo.de>
2022-12-31 18:23:18 +0100

    Add DuplicateIdentifier error

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 use crate::ast::{Identifier, Line, SyntaxError, Tool, Unparsed, Version, Whitespace, AST};
-use std::{fs, io, path::Path, str::Chars};
+use std::{collections::HashSet, fs, io, path::Path, str::Chars};
 
 pub fn parse_file<P: AsRef<Path>>(path: P) -> io::Result<AST> {
     let input = fs::read_to_string(path)?;
@@ -7,10 +7,15 @@ pub fn parse_file<P: AsRef<Path>>(path: P) -> io::Result<AST> {
 }
 
 pub fn parse(input: &str) -> AST {
-    AST(input.lines().map(parse_line).collect())
+    let mut unique_identifiers = HashSet::new();
+
+    AST(input
+        .lines()
+        .map(|line| parse_line(line, &mut unique_identifiers))
+        .collect())
 }
 
-fn parse_line(line: &str) -> Line {
+fn parse_line(line: &str, unique_identifiers: &mut HashSet<String>) -> Line {
     let mut chars = line.chars();
 
     match chars.next() {
@@ -43,11 +48,16 @@ fn parse_line(line: &str) -> Line {
                 },
             }
         }
-        Some(first) => parse_definition(line, first, &mut chars),
+        Some(first) => parse_definition(line, first, &mut chars, unique_identifiers),
     }
 }
 
-fn parse_definition(line: &str, first: char, chars: &mut Chars) -> Line {
+fn parse_definition(
+    line: &str,
+    first: char,
+    chars: &mut Chars,
+    unique_identifiers: &mut HashSet<String>,
+) -> Line {
     let mut tool_name = String::from(first);
 
     match take_identifier(chars, &mut tool_name) {
@@ -65,8 +75,17 @@ fn parse_definition(line: &str, first: char, chars: &mut Chars) -> Line {
             unparsed: Unparsed(line.to_string()),
         },
         Some(next) => {
+            if unique_identifiers.contains(&tool_name) {
+                return Line::Invalid {
+                    error: SyntaxError::DuplicateIdentifier(tool_name),
+                    unparsed: Unparsed(line.to_string()),
+                };
+            }
+
+            unique_identifiers.insert(tool_name.clone());
+
             let name = Identifier(tool_name);
-            let mut versions: Vec<Version> = vec![];
+            let mut versions = Vec::new();
             let mut whitespace = String::from(next);
 
             loop {
